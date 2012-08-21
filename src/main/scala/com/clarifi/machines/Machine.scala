eaefb4cecfaab924d4f46fdbdbbce0d78f24734b
Dan Doel <dan.doel@gmail.com>
2012-08-21 10:29:46 -0400

    Split Profunctor into its own file.

--- src/main/scala/com/clarifi/machines/Machine.scala
+++ src/main/scala/com/clarifi/machines/Machine.scala
@@ -3,19 +3,6 @@ package com.clarifi.machines
 import scalaz._
 import scalaz.syntax.arrow._
 
-sealed trait Profunctor[K[-_, +_]] {
-  def lmap[A, B, C](k: K[A, B])(f: C => A): K[C, B]
-  def rmap[A, B, C](k: K[A, B])(f: B => C): K[A, C]
-}
-
-object Profunctor {
-  implicit object FnProfunctor extends Profunctor[Function1] {
-    type K[A, B] = A => B
-    def lmap[A, B, C](k: K[A, B])(f: C => A): K[C, B] = k compose f
-    def rmap[A, B, C](k: K[A, B])(f: B => C): K[A, C] = k andThen f
-  }
-}
-
 sealed trait Machine[+K[-_, +_], -I, +O] {
   def step: Step[K, I, O, Machine[K, I, O]]
 
---
+++ src/main/scala/com/clarifi/machines/Profunctor.scala
@@ -0,0 +1,15 @@
+package com.clarifi.machines
+
+sealed trait Profunctor[K[-_, +_]] {
+  def lmap[A, B, C](k: K[A, B])(f: C => A): K[C, B]
+  def rmap[A, B, C](k: K[A, B])(f: B => C): K[A, C]
+}
+
+object Profunctor {
+  implicit object FnProfunctor extends Profunctor[Function1] {
+    type K[A, B] = A => B
+    def lmap[A, B, C](k: K[A, B])(f: C => A): K[C, B] = k compose f
+    def rmap[A, B, C](k: K[A, B])(f: B => C): K[A, C] = k andThen f
+  }
+}
+
